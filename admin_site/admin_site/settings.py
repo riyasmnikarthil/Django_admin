5b1e3976529712466f7ab5fdfc9beac1b652b8cf
Muhammad Riyas <riyas@qbadmin-desktop.(none)>
2014-03-19 16:46:50 +0530

    Updated

--- admin_site/admin_site/settings.py
+++ admin_site/admin_site/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'admin_site.mybook',
+    'mybook',
 )
 
 MIDDLEWARE_CLASSES = (
--- admin_site/mybook/admin.py
+++ admin_site/mybook/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import poll
+from .models import poll
 
 class AuthorAdmin(admin.ModelAdmin):
     pass
