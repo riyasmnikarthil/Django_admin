410c15ac3f5ddf63ab087f3ff71ef20a648b6c6d
Muhammad Riyas <riyas@qbadmin-desktop.(none)>
2014-03-20 17:52:34 +0530

    admin done

--- admin_site/mybook/admin.py
+++ admin_site/mybook/admin.py
@@ -1,6 +1,29 @@
-from django.contrib import admin
-from .models import Poll
+from urllib import addbase
+from django.contrib import admin,messages
+from .models import Poll, Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ('status',)
+    fields = (('title','status'),'body')
+    readonly_fields = ['title']
+    ordering = ['title']
+    actions = ['make_published']
+    # filter_horizontal = ('status',)
+    actions_selection_counter = True
+    def make_published(self, request, queryset):
+        rows_updated = queryset.update(status='p')
+        if rows_updated == 1:
+            msg = '1 row was'
+        else:
+            msg = '%s rows were' % rows_updated
+        self.message_user(request, message='%s successfull updated' % msg,level=messages.SUCCESS)
+    make_published.short_description = "Mark selected stories as published"
+
+admin.site.register(Article, ArticleAdmin)
 
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['question','pub_date']
+    date_hierarchy = 'pub_date'
 admin.site.register(Poll, AuthorAdmin)
+# admin.site.disable_action('delete_selected')
--- admin_site/mybook/models.py
+++ admin_site/mybook/models.py
@@ -4,3 +4,17 @@ from django.db import models
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+STATUS_CHOICES = (
+    ('d', 'Draft'),
+    ('p', 'Published'),
+    ('w', 'Withdrawn'),
+)
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+    def __unicode__(self):
+        return self.title
